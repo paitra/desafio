f408a8183dfb330d53eceec0c8b689cea7019b56
paitra <63022256+paitra@users.noreply.github.com>
2023-02-18 11:31:11 -0300

    * Função para salvar os dados no localStorage
    * Recupera os dados já cadastrados no localStorage
    
    * Cria um objeto com os dados do novo produto
    
    * Adiciona o novo produto ao array de produtos
    
    * Salva o array atualizado no localStorage
    
    * Salva os dados no localStorage
    
    * exibe a listagem

--- js/app.js
+++ js/app.js
@@ -12,4 +12,47 @@ $(document).ready(function () {
   $('#' + $(this).data('display')).removeClass('d-none');
   formReset();
   });
+  // Função para salvar os dados no localStorage
+function inserir() {
+  const nome = document.getElementById('nome').value;
+  const unidade = document.getElementById('unidade').value;
+  const quantidade = document.getElementById('quantidade').value;
+  const preco = document.getElementById('preco').value;
+  const perecivel = document.getElementById('perecivel').checked;
+  const validade = document.getElementById('validade').value;
+  const fabricacao = document.getElementById('fabricacao').value;
+
+  // Recupera os dados já cadastrados no localStorage
+  const produtos = JSON.parse(localStorage.getItem('produtos')) || [];
+
+  // Cria um objeto com os dados do novo produto
+  const produto = {
+    nome,
+    unidade,
+    quantidade,
+    preco,
+    perecivel,
+    validade,
+    fabricacao
+  };
+
+  // Adiciona o novo produto ao array de produtos
+  produtos.push(produto);
+
+  // Salva o array atualizado no localStorage
+  localStorage.setItem('produtos', JSON.stringify(produtos));
+}
+$(document).on('submit', 'form', (event) => {
+  event.preventDefault();
+ 
+  // Salva os dados no localStorage
+  if ($('input[name=edit]').length > 0) {
+    atualizar($('input[name=edit]').val());
+  } else {
+    inserir();
+  }
+  carregarProdutos();
+  //exibe a listagem
+  $('#list-action').trigger('click');
+});
   });
