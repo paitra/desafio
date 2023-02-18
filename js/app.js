ae8a7ea67557a548da32d7c06acc7f1388bb27e0
paitra <63022256+paitra@users.noreply.github.com>
2023-02-18 11:08:17 -0300

    adicionado funçao de menu retratio

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
-//atributos personalizados (data-display e data-hidden)
-$('.menu').on('click', function (e) {
+ //Menu Retratio
+$(document).ready(function () {
+
+  $('#sidebarCollapse').on('click', function () {
+  $('#sidebar').toggleClass('active');
+  $(this).toggleClass('active');
+  });  
+  //atributos personalizados (data-display e data-hidden)
+  $('.menu').on('click', function (e) {
   e.preventDefault();
   $('#' + $(this).data('hidden')).addClass('d-none');
   $('#' + $(this).data('display')).removeClass('d-none');
   formReset();
-});
+  });
+  });
